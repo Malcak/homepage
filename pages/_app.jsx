6de3b75af1f664710fa38d03058284a0097d5871
Malcak <malcakx@gmail.com>
2021-10-28 12:25:07 -0500

    perf(fonts): change font provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,14 @@
-import '@fontsource/m-plus-rounded-1c'
-
 import { ChakraProvider } from '@chakra-ui/react'
 import { AnimatePresence } from 'framer-motion'
 
+import Fonts from '../components/fonts'
 import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <AnimatePresence exitBeforeEnter initial={true}>
           <Component {...pageProps} key={router.route} />
