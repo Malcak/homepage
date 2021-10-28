be64acaf10b65238869f1fd0629d429ab5d30e25
Malcak <malcakx@gmail.com>
2021-10-27 19:30:00 -0500

    style: sort styled css props

--- components/logo.jsx
+++ components/logo.jsx
@@ -4,17 +4,13 @@ import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
+  align-items: center;
+  display: inline-flex;
   font-weight: bold;
   font-size: 18px;
-  display: inline-flex;
-  align-items: center;
   height: 30px;
   line-height: 20px;
   padding: 10px;
-
-  img {
-    transition: 200ms ease;
-  }
 `
 
 const Logo = () => {
