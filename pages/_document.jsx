5b3ccd15a529946784b68f149e04036257225538
Malcak <malcakx@gmail.com>
2021-11-17 16:20:53 -0500

    refactor: restore scrollbar

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,21 +1,7 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import styled from '@emotion/styled'
 import { ColorModeScript } from '@chakra-ui/react'
 
 import theme from '../lib/theme'
-
-// removing scrollbars from body
-const Body = styled.body`
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  /* Hide scrollbar for IE, Edge and Firefox */
-  & {
-    -ms-overflow-style: none; /* IE and Edge */
-    scrollbar-width: none; /* Firefox */
-  }
-`
 
 export default class Document extends NextDocument {
   render() {
@@ -23,12 +9,12 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head />
 
-        <Body>
+        <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
 
           <Main />
           <NextScript />
-        </Body>
+        </body>
       </Html>
     )
   }
